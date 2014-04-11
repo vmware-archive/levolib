21b0c9dc1768303dd1650ad025ad9e4b29eaded9
Aaron Jarecki <ajarecki@pivotallabs.com>
2014-04-11 13:29:16 -0400

    Aaron: quick change to schema process

--- json-schema-adapter.go
+++ json-schema-adapter.go
@@ -23,7 +23,6 @@ import (
 type JSONSchemaAdapter struct{}
 
 func (self *JSONSchemaAdapter) ProcessSchemaFile(schemaPath string) (Schema, error) {
-	fmt.Printf("")
 	fileContents, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
 		return Schema{}, err
@@ -32,6 +31,7 @@ func (self *JSONSchemaAdapter) ProcessSchemaFile(schemaPath string) (Schema, err
 }
 
 func (self *JSONSchemaAdapter) ParseModelSchemaString(schemaString []byte) (Schema, error) {
+	fmt.Printf("")
 	var schema Schema
 	err := json.Unmarshal(schemaString, &schema)
 	if err != nil {
@@ -40,10 +40,10 @@ func (self *JSONSchemaAdapter) ParseModelSchemaString(schemaString []byte) (Sche
 	if err := schema.validate(); err != nil {
 		return Schema{}, err
 	}
-	for _, model := range schema.Models {
-		for _, prop := range model.Properties {
+	for modelIndex, model := range schema.Models {
+		for propIndex, prop := range model.Properties {
 			if prop.LocalIdentifier == "" {
-				prop.LocalIdentifier = prop.RemoteIdentifier
+				schema.Models[modelIndex].Properties[propIndex].LocalIdentifier = prop.RemoteIdentifier
 			}
 		}
 	}
